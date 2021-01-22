e60fa7e52f9d7e4050b99fbfee6da8911f106d68
ticochuck <chucklivilla@gmail.com>
2021-01-22 12:27:16 -0800

    views created

--- weights/views.py
+++ weights/views.py
@@ -1,3 +1,36 @@
+from django import forms
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import (CreateView, DeleteView, DetailView, ListView,
+                                  UpdateView)
 
-# Create your views here.
+from .models import Weights
+
+
+class HomePageView(ListView):
+    template_name = 'home.html'
+    model = Weights
+    fields = '__all__'
+
+
+class CreatePageView(CreateView):
+    template_name = 'home.html'
+    model = Weights
+    fields = '__all__'
+
+
+class DetailPageView(DetailView):
+    template_name = 'home.html'
+    model = Weights
+
+
+class UpdatePageView(UpdateView):
+    template_name = 'home.html'
+    model = Weights
+    fields = '__all__'
+
+
+class DeletePageView(DetailView):
+    template_name = 'home.html'
+    model = Weights
+    success_url = reverse_lazy('home')
